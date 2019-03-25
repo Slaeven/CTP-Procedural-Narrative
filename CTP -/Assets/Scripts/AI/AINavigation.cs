4f30e20516bdf24d1def5fd5aa1335b870e697ee
Unknown <thomas4.harrison@live.uwe.ac.uk>
2019-03-25 14:38:22 +0000

    Store words now

--- CTP -/Assets/Scripts/AI/AINavigation.cs
+++ CTP -/Assets/Scripts/AI/AINavigation.cs
@@ -10,6 +10,8 @@ public class AINavigation : MonoBehaviour
     public GameObject AInpc;
     public GameObject moveTo;
     public string[] lines;
+    public string[,] locs;
+    public string[] words;
 
     void Awake()
     {
@@ -21,8 +23,10 @@ public class AINavigation : MonoBehaviour
     {
         string path1 = Application.dataPath + "/output1.txt";
         lines = System.IO.File.ReadAllLines(path1); // Will include empty lines. Also when importing the txt remove the numbered lines
-
-	}
+        StartCoroutine("WaitForSpawn");
+        locs = new string[50, 50];
+        //words = new string[100];
+    }
 
     public void MoveToLocation(Vector3 targetPoint)
     {
@@ -35,4 +39,45 @@ public class AINavigation : MonoBehaviour
 
         MoveToLocation(moveTo.transform.position);
 	}
+
+    void Decypher()
+    {
+        for (int i = 0; i < lines.Length; i++) 
+        {
+            words = lines[i].Split(" "[0]); // Splits lines into words
+        }
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (words[j].Equals("move"))
+                {
+                    string firstLoc = words[j + 2]; // Stores first move location
+                    string secondLoc = words[j + 3]; // Stores second move location
+                    Storage("move", firstLoc, secondLoc);
+                }
+            }
+        }
+       
+
+    }
+
+    void Storage(string action, string firstLoc, string secondLoc) // 2d array storing all movements
+    {
+        int i = 0;
+
+        locs[i, 0] = firstLoc;
+        locs[i, 1] = secondLoc;
+
+        i++;
+
+    }
+
+    IEnumerator WaitForSpawn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Decypher();
+
+        yield return null;
+    }
 }
